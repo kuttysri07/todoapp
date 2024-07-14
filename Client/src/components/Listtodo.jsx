7d523431a12239cf66850ad4413dbc0518ddae9b
Sriram.T <144159691+kuttysri07@users.noreply.github.com>
2024-07-14 10:10:31 +0530

    Update Listtodo.jsx

--- Client/src/components/Listtodo.jsx
+++ Client/src/components/Listtodo.jsx
@@ -1,77 +1,65 @@
-
-import React from 'react'
-import { useState } from 'react';
-import { useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import Edittodo from './Edittodo';
+
 const apiUrl = process.env.REACT_APP_API_URL;
 
-
-
-
-
-
 const Listtodo = () => {
+    const [todos, setTodos] = useState([]);
 
-    const [todos,setTodos]=useState([])
-
-   
-    async function deleteTodo(id){
+    async function deleteTodo(id) {
         try {
-            const deleteTodo=await fetch(`${apiUrl}/todos/${id}`,{
-                method:"DELETE"
+            const response = await fetch(`${apiUrl}/todos/${id}`, {
+                method: "DELETE"
             });
-           setTodos( todos.filter(todo=>{
-                return (todo.todo_id !== id)
-           }))
-            console.log(deleteTodo);
-
+            if (!response.ok) {
+                throw new Error('Failed to delete todo');
+            }
+            setTodos(todos.filter(todo => todo.todo_id !== id));
+            console.log(`Todo with ID ${id} deleted successfully`);
         } catch (err) {
-            console.log(err.message);
+            console.error(err.message);
         }
     }
 
-
-async function getTodos(){
-    try {
-        const response=await fetch(`${apiUrl}/todos`);
-        const jsonData= await response.json()
-        setTodos(jsonData)
-       
-    } catch (err) {
-        console.error(err.message);
+    async function getTodos() {
+        try {
+            const response = await fetch(`${apiUrl}/todos`);
+            if (!response.ok) {
+                throw new Error('Failed to fetch todos');
+            }
+            const jsonData = await response.json();
+            setTodos(jsonData);
+        } catch (err) {
+            console.error(err.message);
+        }
     }
 
+    useEffect(() => {
+        getTodos();
+    }, []);
+
+    return (
+        <div>
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th scope="col">Description</th>
+                        <th scope="col">Edit</th>
+                        <th scope="col">Delete</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {todos.map(todo => (
+                        <tr key={todo.todo_id}>
+                            <td>{todo.description}</td>
+                            <td><Edittodo todo={todo} /></td>
+                            <td className='btn btn-danger' onClick={() => deleteTodo(todo.todo_id)}>Delete</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
 }
 
-useEffect(()=>{
-    getTodos()
-    console.log('API URL:', process.env.REACT_APP_API_URL);
-},[])
-console.log(todos)
-  return (
-    <div>
-        <table className="table">
-  <thead>
-    <tr>
-     
-      <th scope="col">Description</th>
-      <th scope="col">Edit</th>
-      <th scope="col">Delete</th>
-    </tr>
-  </thead>
-  <tbody>
-    {todos.map(todo=>{
-       return(
-       <tr key={todo.todo_id}>
-            <td>{todo.description}</td>
-            <td><Edittodo todo={todo}/></td>
-            <td className='btn btn-danger' onClick={()=> deleteTodo(todo.todo_id)} >Delete</td>
-      </tr>) 
-    })}
-  </tbody>
-</table>
-    </div>
-  )
-}
-
-export default Listtodo
+export default Listtodo;
